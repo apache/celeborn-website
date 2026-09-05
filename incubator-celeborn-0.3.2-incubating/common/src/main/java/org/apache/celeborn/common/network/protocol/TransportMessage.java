6d65c5da58e543eb09419654fc0ffd7a3bdd4d4a
Celeborn <dev@celeborn.apache.org>
2026-09-05 01:07:52 +0000

    docs/0.3.2-incubating

---
+++ incubator-celeborn-0.3.2-incubating/common/src/main/java/org/apache/celeborn/common/network/protocol/TransportMessage.java
@@ -0,0 +1,118 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.celeborn.common.network.protocol;
+
+import static org.apache.celeborn.common.protocol.MessageType.BACKLOG_ANNOUNCEMENT_VALUE;
+import static org.apache.celeborn.common.protocol.MessageType.BUFFER_STREAM_END_VALUE;
+import static org.apache.celeborn.common.protocol.MessageType.OPEN_STREAM_VALUE;
+import static org.apache.celeborn.common.protocol.MessageType.PUSH_DATA_HAND_SHAKE_VALUE;
+import static org.apache.celeborn.common.protocol.MessageType.READ_ADD_CREDIT_VALUE;
+import static org.apache.celeborn.common.protocol.MessageType.REGION_FINISH_VALUE;
+import static org.apache.celeborn.common.protocol.MessageType.REGION_START_VALUE;
+import static org.apache.celeborn.common.protocol.MessageType.STREAM_HANDLER_VALUE;
+
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+
+import com.google.protobuf.GeneratedMessageV3;
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.celeborn.common.exception.CelebornIOException;
+import org.apache.celeborn.common.protocol.MessageType;
+import org.apache.celeborn.common.protocol.PbBacklogAnnouncement;
+import org.apache.celeborn.common.protocol.PbBufferStreamEnd;
+import org.apache.celeborn.common.protocol.PbOpenStream;
+import org.apache.celeborn.common.protocol.PbPushDataHandShake;
+import org.apache.celeborn.common.protocol.PbReadAddCredit;
+import org.apache.celeborn.common.protocol.PbRegionFinish;
+import org.apache.celeborn.common.protocol.PbRegionStart;
+import org.apache.celeborn.common.protocol.PbStreamHandler;
+
+public class TransportMessage implements Serializable {
+  private static final long serialVersionUID = -3259000920699629773L;
+  private static Logger logger = LoggerFactory.getLogger(TransportMessage.class);
+  @Deprecated private final MessageType type;
+  private final int messageTypeValue;
+  private final byte[] payload;
+
+  public TransportMessage(MessageType type, byte[] payload) {
+    this.type = type;
+    this.messageTypeValue = type.getNumber();
+    this.payload = payload;
+  }
+
+  public MessageType getType() {
+    return type;
+  }
+
+  public int getMessageTypeValue() {
+    return messageTypeValue;
+  }
+
+  public byte[] getPayload() {
+    return payload;
+  }
+
+  public <T extends GeneratedMessageV3> T getParsedPayload() throws InvalidProtocolBufferException {
+    switch (messageTypeValue) {
+      case OPEN_STREAM_VALUE:
+        return (T) PbOpenStream.parseFrom(payload);
+      case STREAM_HANDLER_VALUE:
+        return (T) PbStreamHandler.parseFrom(payload);
+      case BACKLOG_ANNOUNCEMENT_VALUE:
+        return (T) PbBacklogAnnouncement.parseFrom(payload);
+      case BUFFER_STREAM_END_VALUE:
+        return (T) PbBufferStreamEnd.parseFrom(payload);
+      case READ_ADD_CREDIT_VALUE:
+        return (T) PbReadAddCredit.parseFrom(payload);
+      case PUSH_DATA_HAND_SHAKE_VALUE:
+        return (T) PbPushDataHandShake.parseFrom(payload);
+      case REGION_START_VALUE:
+        return (T) PbRegionStart.parseFrom(payload);
+      case REGION_FINISH_VALUE:
+        return (T) PbRegionFinish.parseFrom(payload);
+      default:
+        logger.error("Unexpected type {}", type);
+    }
+    return null;
+  }
+
+  public ByteBuffer toByteBuffer() {
+    int totalBufferSize = payload.length + 4 + 4;
+    ByteBuffer buffer = ByteBuffer.allocate(totalBufferSize);
+    buffer.putInt(messageTypeValue);
+    buffer.putInt(payload.length);
+    buffer.put(payload);
+    buffer.flip();
+    return buffer;
+  }
+
+  public static TransportMessage fromByteBuffer(ByteBuffer buffer) throws CelebornIOException {
+    int messageTypeValue = buffer.getInt();
+    if (MessageType.forNumber(messageTypeValue) == null) {
+      throw new CelebornIOException("Decode failed, fallback to legacy messages.");
+    }
+    int payloadLen = buffer.getInt();
+    byte[] payload = new byte[payloadLen];
+    buffer.get(payload);
+    MessageType msgType = MessageType.forNumber(messageTypeValue);
+    return new TransportMessage(msgType, payload);
+  }
+}
